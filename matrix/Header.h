78aac54893509ee447a1abf3ad66b0a56de0891d
hork <horkworse@gmail.com>
2020-05-28 18:16:51 +0500

    fix linked list

--- matrix/Header.h
+++ matrix/Header.h
@@ -1,413 +1,285 @@
 #pragma once
+#include <time.h>
 
 class Matrix3x3
 {
-    public:
-        Matrix3x3()
-        {
-            values = new int* [size];
+public:
+    Matrix3x3()
+    {
+        values = new int* [size];
 
-            for (int i = 0; i < size; i++)
-            {
-                values[i] = new int[size];
-                for (int j = 0; j < size; j++)
-                    values[i][j] = 0;
-            }
-        }
-        ~Matrix3x3()
+        for (int i = 0; i < size; i++)
         {
-            for (int i = 0; i < size; i++)
-                delete[] values[i];
-            delete[] values;
+            values[i] = new int[size];
+            for (int j = 0; j < size; j++)
+                values[i][j] = 0;
         }
+    }
+    ~Matrix3x3()
+    {
+        for (int i = 0; i < size; i++)
+            delete[] values[i];
+        delete[] values;
+    }
 
-        const int element(const int i, const int j)
-        {
-            return values[i][j];
-        }
+    const int element(const int i, const int j)
+    {
+        return values[i][j];
+    }
 
-        void setElement(const int i, const int j, const int value)
-        {
-            values[i][j] = value;
-        }
+    void setElement(const int i, const int j, const int value)
+    {
+        values[i][j] = value;
+    }
 
-        void fillRandomElements(const int minVal, const int maxVal)
-        {
-            srand(time(NULL));
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    values[i][j] = rand() % (maxVal * 2 ) + minVal;
-        }
+    void fillRandomElements(const int minVal, const int maxVal)
+    {
+        srand(time(NULL));
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                values[i][j] = rand() % (maxVal * 2) + minVal;
+    }
 
-        void print()
+    void print()
+    {
+        srand(time(NULL));
+        for (int i = 0; i < size; i++)
         {
-            srand(time(NULL));
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                    std::cout << values[i][j] << '\t';
-                std::cout << std::endl;
-            }
+            for (int j = 0; j < size; j++)
+                std::cout << values[i][j] << '\t';
             std::cout << std::endl;
         }
+        std::cout << std::endl;
+    }
 
-        const int sumPrincipalDiag()
-        {
-            int sum = 0;
-            for (int i = 0; i < size; i++)
-                sum += values[i][i];
-            return sum;
-        }
+    const int sumPrincipalDiag()
+    {
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += values[i][i];
+        return sum;
+    }
 
-        const int sumSecondaryDiag()
-        {
-            int sum = 0;
-            for (int i = 0; i < size; i++)
-                sum += values[i][2 - i];
-            return sum;
-        }
+    const int sumSecondaryDiag()
+    {
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += values[i][2 - i];
+        return sum;
+    }
 
-        const int productPrincipalDiag()
-        {
-            int result = 1;
-            for (int i = 0; i < size; i++)
-                result *= values[i][i];
-            return result;
-        }
+    const int productPrincipalDiag()
+    {
+        int result = 1;
+        for (int i = 0; i < size; i++)
+            result *= values[i][i];
+        return result;
+    }
 
-        const int productSecondaryDiag()
-        {
-            int result = 1;
-            for (int i = 0; i < size; i++)
-                result *= values[i][2 - i];
-            return result;
-        }
+    const int productSecondaryDiag()
+    {
+        int result = 1;
+        for (int i = 0; i < size; i++)
+            result *= values[i][2 - i];
+        return result;
+    }
 
-        const int sumRow(const int iRow)
-        {
-            int sum = 0;
-            for (int j; j < size; j++)
-                sum = values[iRow][j];
-            return sum;
-        }
+    const int sumRow(const int iRow)
+    {
+        int sum = 0;
+        for (int j; j < size; j++)
+            sum = values[iRow][j];
+        return sum;
+    }
 
-        const int minColumn(const int iCol)
-        {
-                int min = values[0][iCol];
+    const int minColumn(const int iCol)
+    {
+        int min = values[0][iCol];
 
-                for (int i = 1; i < size; i++)
-                    if (min > values[i][iCol])
-                        min = values[i][iCol];
-                return min;
-        }
+        for (int i = 1; i < size; i++)
+            if (min > values[i][iCol])
+                min = values[i][iCol];
+        return min;
+    }
 
-        const int maxColumn(const int iCol)
-        {
-            int max = values[0][iCol];
+    const int maxColumn(const int iCol)
+    {
+        int max = values[0][iCol];
 
-            for (int i = 1; i < size; i++)
-                if (max < values[i][iCol])
-                    max = values[i][iCol];
-            return max;
-        }
-           
+        for (int i = 1; i < size; i++)
+            if (max < values[i][iCol])
+                max = values[i][iCol];
+        return max;
+    }
 
-    private:
-        int **values;
-        int size = 3;
+
+private:
+    int** values;
+    int size = 3;
 };
 
 class MatrixXnX
 {
-    public:
-        MatrixXnX(const int iDim)
-        {
-            this->size = iDim;
-            values = new int* [iDim];
+public:
+    MatrixXnX(const int iDim)
+    {
+        this->size = iDim;
+        values = new int* [iDim];
 
-            for (int i = 0; i < iDim; i++)
-            {
-                values[i] = new int[iDim];
-                for (int j = 0; j < iDim; j++)
-                    values[i][j] = 0;
-            }
-        }
-        ~MatrixXnX()
+        for (int i = 0; i < iDim; i++)
         {
-            for (int i = 0; i < size; i++)
-                delete[] values[i];
-            delete[] values;
+            values[i] = new int[iDim];
+            for (int j = 0; j < iDim; j++)
+                values[i][j] = 0;
         }
+    }
+    ~MatrixXnX()
+    {
+        for (int i = 0; i < size; i++)
+            delete[] values[i];
+        delete[] values;
+    }
 
-        const int element(const int i, const int j)
-        {
-            return values[i][j];
-        }
+    const int element(const int i, const int j)
+    {
+        return values[i][j];
+    }
 
-        void setElement(const int i, const int j, const int value)
-        {
-            values[i][j] = value;
-        }
+    void setElement(const int i, const int j, const int value)
+    {
+        values[i][j] = value;
+    }
 
-        void fillRandomElements(const int minVal, const int maxVal)
-        {
-            srand(time(NULL));
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    values[i][j] = rand() % (maxVal * 2 ) + minVal;
-        }
+    void fillRandomElements(const int minVal, const int maxVal)
+    {
+        srand(time(NULL));
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                values[i][j] = rand() % (maxVal * 2) + minVal;
+    }
 
-        void print()
+    void print()
+    {
+        srand(time(NULL));
+        for (int i = 0; i < size; i++)
         {
-            srand(time(NULL));
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                    std::cout << values[i][j] << "  ";
-                std::cout << std::endl;
-            }
+            for (int j = 0; j < size; j++)
+                std::cout << values[i][j] << "  ";
             std::cout << std::endl;
         }
+        std::cout << std::endl;
+    }
 
-        const int sumPrincipalDiag()
-        {
-            int sum = 0;
-            for (int i = 0; i < size; i++)
-                sum += values[i][i];
-            return sum;
-        }
+    const int sumPrincipalDiag()
+    {
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += values[i][i];
+        return sum;
+    }
 
-        const int sumSecondaryDiag()
-        {
-            int sum = 0;
-            for (int i = 0; i < size; i++)
-                sum += values[i][size - 1 - i];
-            return sum;
-        }
+    const int sumSecondaryDiag()
+    {
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += values[i][size - 1 - i];
+        return sum;
+    }
 
-        const int productPrincipalDiag()
-        {
-            int result = 1;
-            for (int i = 0; i < size; i++)
-                result *= values[i][i];
-            return result;
-        }
+    const int productPrincipalDiag()
+    {
+        int result = 1;
+        for (int i = 0; i < size; i++)
+            result *= values[i][i];
+        return result;
+    }
 
-        const int productSecondaryDiag()
-        {
-            int result = 1;
-            for (int i = 0; i < size; i++)
-                result *= values[i][size - 1 - i];
-            return result;
-        }
+    const int productSecondaryDiag()
+    {
+        int result = 1;
+        for (int i = 0; i < size; i++)
+            result *= values[i][size - 1 - i];
+        return result;
+    }
 
-        const int sumRow(const int iRow)
-        {
-            int sum = 0;
-            for (int j; j < size; j++)
-                sum = values[iRow][j];
-            return sum;
-        }
-        
-        const int minColumn(const int iCol)
-        {
-            int min = values[0][iCol];
+    const int sumRow(const int iRow)
+    {
+        int sum = 0;
+        for (int j; j < size; j++)
+            sum = values[iRow][j];
+        return sum;
+    }
 
-            for (int i = 1; i < size; i++)
-                if (min > values[i][iCol])
-                    min = values[i][iCol];
-            return min;
-        }
+    const int minColumn(const int iCol)
+    {
+        int min = values[0][iCol];
 
-        const int maxColumn(const int iCol)
-        {
-            int max = values[0][iCol];
+        for (int i = 1; i < size; i++)
+            if (min > values[i][iCol])
+                min = values[i][iCol];
+        return min;
+    }
 
-            for (int i = 1; i < size; i++)
-                if (max < values[i][iCol])
-                    max = values[i][iCol];
-            return max;
-        }
+    const int maxColumn(const int iCol)
+    {
+        int max = values[0][iCol];
 
-    private:
-        int **values;
-        int size;
+        for (int i = 1; i < size; i++)
+            if (max < values[i][iCol])
+                max = values[i][iCol];
+        return max;
+    }
+
+private:
+    int** values;
+    int size;
 };
 
 class linkedList
 {
-	public:
-		linkedList()
-		{
-			this->_head = nullptr;
-			this->_tail = nullptr;
-			this->_count = 0;
-		};
+public:
+    linkedList();
 
-		~linkedList()
-		{
-			clear();
-		}
+    ~linkedList();
 
-		void add(int value)
-		{
-			ListNode* temp = new ListNode;
-			temp->value = value;
-			temp->next = nullptr;
+    void add(int value);
 
-			if (_head == nullptr)
-			{
-				_head = temp;
-				_head->next = nullptr;
-			}
-			else
-				_tail->next = temp;
-			_tail = temp;
-			this->_count++;
-		}
+    bool contains(int value);
 
-		bool contains(int value)
-		{
-			ListNode* current = _head;
-			ListNode* previous = new ListNode;
+    int count();
 
-			while (current->next != nullptr)
-			{
-				previous = current;
-				current = current->next;
+    void removeDuplicates();
 
-				if (current->value == value)
-					return true;
-			}
-			return false;
-		}
+    int findFromBack(int index);
 
-		int count()
-		{
-			return this->_count;
-		}		
+    bool remove(int value);
 
-		void removeDuplicates()
-		{
-			if (_head == nullptr)
-				return;
-			
-			ListNode* current = _head;
-			
-			while (current != nullptr)
-			{
-				ListNode* runner = current;
-				while (runner->next != nullptr)
-				{
-					if (runner->next->value == current->value)
-					{
-						ListNode* temp = runner->next->next;
-						delete runner->next;
-						runner->next = nullptr;
-						_count--;
-						if (temp != nullptr)
-							runner->next = temp;
-					}
-					else
-						runner = runner->next;
-				}
-				current = current->next;
-			}
-		}
+    void clear();
 
-		int findFromBack(int index)
-		{
-			if (index > _count)
-				return -1;
+    void print();
 
-			int counter = 0;
-			int itemIndex = this->_count - index;
+private:
 
-			ListNode* temp = _head;
-			while (counter != itemIndex)
-			{
-				temp = temp->next;
-				counter++;
-			}
-			return temp->value;
-		}
+    class ListNode
+    {
+    public:
+        ListNode()
+        {
+            this->value = 0;
+            this->next = nullptr;
+        };
 
-		bool remove(int value)
-		{
-			ListNode* current = _head;
-			ListNode* previous = new ListNode;
+        ListNode(int value, ListNode* next)
+        {
+            this->value = value;
+            this->next = next;
+        };
 
-			while (current->next != nullptr)
-			{
-				previous = current;
-				current = current->next;
+        int value;
+        ListNode* next;
+    };
 
-				if (current->value == value)
-				{
-					previous->next = current->next;
-					delete current;
-					_count--;
-					return true;
-				}
-			}
-			return false;
-		}
-
-		void clear()
-		{
-			if (_head == nullptr)
-				return;
-
-			ListNode* tmp;
-			while (_head != nullptr)
-			{
-				tmp = _head-> next;
-				delete _head;
-				_head = tmp;
-				_count = 0;
-			}
-		}
-
-		void print()
-		{		
-			if (_head == nullptr)
-			{
-				cout << "none";
-				return;
-			}
-
-			ListNode* temp = _head;
-
-			while (temp != nullptr)
-			{
-				cout << temp->value << "  ";
-				temp = temp->next;
-			}
-			cout << endl;
-		}
-
-	private:
-
-		class ListNode
-		{
-			public:
-				ListNode()
-				{
-					this->value = 0;
-					this->next = nullptr;
-				};
-
-				ListNode(int value, ListNode* next)
-				{
-					this->value = value;
-					this->next = next;
-				};
-
-				int value;
-				ListNode* next;
-		};
-
-		ListNode* _head;
-		ListNode* _tail;
-		int _count;
+    ListNode* _head;
+    ListNode* _tail;
+    int _count;
 };
--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,54 +1,205 @@
-// 280002 Минеев Е.С. 1 задание
+// matrixList.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
 
 #include <iostream>
+#include "Header.h"
 
 using namespace std;
 
+linkedList::linkedList()
+{
+    this->_head = nullptr;
+    this->_tail = nullptr;
+    this->_count = 0;
+};
+
+linkedList::~linkedList()
+{
+    clear();
+}
+
+void linkedList::add(int value)
+{
+    ListNode* temp = new ListNode;
+    temp->value = value;
+    temp->next = nullptr;
+
+    if (_head == nullptr)
+    {
+        _head = temp;
+        _head->next = nullptr;
+    }
+    else
+        _tail->next = temp;
+    _tail = temp;
+    this->_count++;
+}
+
+bool linkedList::contains(int value)
+{
+    ListNode* current = _head;
+    ListNode* previous = new ListNode;
+
+    while (current->next != nullptr)
+    {
+        previous = current;
+        current = current->next;
+
+        if (current->value == value)
+            return true;
+    }
+    return false;
+}
+
+int linkedList::count()
+{
+    return this->_count;
+}
+
+void linkedList::removeDuplicates()
+{
+    if (_head == nullptr)
+        return;
+
+    ListNode* current = _head;
+
+    while (current != nullptr)
+    {
+        ListNode* runner = current;
+        while (runner->next != nullptr)
+        {
+            if (runner->next->value == current->value)
+            {
+                ListNode* temp = runner->next->next;
+                delete runner->next;
+                runner->next = nullptr;
+                _count--;
+                if (temp != nullptr)
+                    runner->next = temp;
+            }
+            else
+                runner = runner->next;
+        }
+        current = current->next;
+    }
+}
+
+int linkedList::findFromBack(int index)
+{
+    if (index > _count)
+        return -1;
+
+    int counter = 0;
+    int itemIndex = this->_count - index;
+
+    ListNode* temp = _head;
+    while (counter != itemIndex)
+    {
+        temp = temp->next;
+        counter++;
+    }
+    return temp->value;
+}
+
+bool linkedList::remove(int value)
+{
+    ListNode* current = _head;
+    ListNode* previous = new ListNode;
+
+    while (current->next != nullptr)
+    {
+        previous = current;
+        current = current->next;
+
+        if (current->value == value)
+        {
+            previous->next = current->next;
+            delete current;
+            _count--;
+            return true;
+        }
+    }
+    return false;
+}
+
+void linkedList::clear()
+{
+    if (_head == nullptr)
+        return;
+
+    ListNode* tmp;
+    while (_head != nullptr)
+    {
+        tmp = _head->next;
+        delete _head;
+        _head = tmp;
+        _count = 0;
+    }
+}
+
+void linkedList::print()
+{
+    if (_head == nullptr)
+    {
+        cout << "none";
+        return;
+    }
+
+    ListNode* temp = _head;
+
+    while (temp != nullptr)
+    {
+        cout << temp->value << "  ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 int main()
 {
-	// Работа с матрицей 3х3
-	Matrix3x3 matrix;
+    // Работа с матрицей 3х3
+	Matrix3x3 matrix3x3;
 	// заполняем случайными элементами
-    matrix.fillRandomElements(-10, 10);
+    matrix3x3.fillRandomElements(-10, 10);
     // выводим
-    matrix.print();
+    matrix3x3.print();
 
 	// вычисляем минимальный элемент в столбце 0
-    cout << matrix.minColumn(0) << endl;
+    cout << "minimal in 0 column is " << matrix3x3.minColumn(0) << endl;
     // вычисляем максимальный элемент в столбце 0
-    cout << matrix.maxColumn(0) << endl;
+    cout << "maximal in 0 column is "  << matrix3x3.maxColumn(0) << endl;
 
 	// вычисляем сумму элементов главной диагонали
-    cout << matrix.sumPrincipalDiag() << endl;
+    cout << "sum principal diag is " << matrix3x3.sumPrincipalDiag() << endl;
     // вычисляем сумму элементов побочной диагонали
-    cout << matrix.sumSecondaryDiag() << endl;
+    cout << "sum secondary diag is " << matrix3x3.sumSecondaryDiag() << endl;
 
     // вычисляем произведение элементов главной диагонали
-    cout << matrix.productPrincipalDiag() << endl;
+    cout << "product principal diag is " << matrix3x3.productPrincipalDiag() << endl;
     // вычисляем произведение элементов побочной диагонали
-    cout << matrix.productSecondaryDiag() << endl;
+    cout << "product secondary diag is " << matrix3x3.productSecondaryDiag() << endl;
 
     // Работа с матрицей nхn
-    MatrixXnX matrix(4);
+    MatrixXnX matrix4x4(4);
     // заполняем случайными элементами
-    matrix.fillRandomElements(-10, 10);
+    matrix4x4.fillRandomElements(-10, 10);
     // выводим
-    matrix.print();
+    matrix4x4.print();
 
     // вычисляем минимальный элемент в столбце 0
-    cout << matrix.minColumn(0) << endl;
+    cout << "minimal in 0 column is " << matrix4x4.minColumn(0) << endl;
     // вычисляем максимальный элемент в столбце 0
-    cout << matrix.maxColumn(0) << endl;
+    cout << "maximal in 0 column is " << matrix4x4.maxColumn(0) << endl;
 
     // вычисляем сумму элементов главной диагонали
-    cout << matrix.sumPrincipalDiag() << endl;
+    cout << "sum principal diag is " << matrix4x4.sumPrincipalDiag() << endl;
     // вычисляем сумму элементов побочной диагонали
-    cout << matrix.sumSecondaryDiag() << endl;
+    cout << "sum secondary diag is " << matrix4x4.sumSecondaryDiag() << endl;
 
     // вычисляем произведение элементов главной диагонали
-    cout << matrix.productPrincipalDiag() << endl;
+    cout << "product principal diag is " << matrix4x4.productPrincipalDiag() << endl;
     // вычисляем произведение элементов побочной диагонали
-    cout << matrix.productSecondaryDiag() << endl;
+    cout << "product secondary diag is " << matrix4x4.productSecondaryDiag() << endl << endl;
 
 
 	// Работа со связным списком
@@ -70,5 +221,5 @@ int main()
 	// Удаляем дубликаты
 	list.removeDuplicates();
 	// выводим
-	list.print();
+	list.print();   
 }
