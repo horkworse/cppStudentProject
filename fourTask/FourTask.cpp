3c0dac2070dd3fb985873f088287e45f83236f53
hork <horkworse@gmail.com>
2020-05-28 14:48:42 +0500

    fix bsearch

--- fourTask/FourTask.cpp
+++ fourTask/FourTask.cpp
@@ -18,71 +18,79 @@ int main()
         bigArray[i] = rand() % 2001 - 1000;
 
     // Search
+    cout << "//////////Search//////////" << endl << endl;
     cout << "Search function:" << endl;
     cout << "Element with 20 value is on " << Search(bigArray, n, 20) << endl << endl << endl;
 
     // Quick sort
-    
+    cout << "//////////Quick sort//////////" << endl << endl;
     int m = 100;
     int* smallArray = new int[m];
     int* otherSmallArray = new int[m];
 
     srand(time(nullptr));
 
-     for (int i = 0; i < m; i++)
-     {
-         smallArray[i] = rand() % 21 - 10;
-         otherSmallArray[i] = smallArray[i];
-     }
+    for (int i = 0; i < m; i++)
+    {
+        smallArray[i] = rand() % 21 - 10;
+        otherSmallArray[i] = smallArray[i];
+    }
 
-     cout << "Quick sort with recursion:" << endl;
-     cout << "Inital array:" << endl;
-     Print(smallArray, m / 2);
-     QuickSortReq(smallArray, 0, m - 1);
-     cout << "Sorted array:" << endl;
-     Print(smallArray, m / 2);
+    cout << "Quick sort with recursion:" << endl << endl;
+    cout << "Inital array:" << endl;
+    Print(smallArray, m / 2);
+    QuickSortReq(smallArray, 0, m - 1);
+    cout << "Sorted array:" << endl;
+    Print(smallArray, m / 2);
 
-     cout << endl << endl;
+    cout << endl;
 
-     cout << "Quick sort:" << endl;
-     cout << "Inital array:" << endl;
-     Print(otherSmallArray, m / 2);
-     QuickSort(otherSmallArray, 0, m - 1);
-     cout << "Sorted array:" << endl;
-     Print(otherSmallArray, m / 2);
+    cout << "Quick sort:" << endl << endl;
+    cout << "Inital array:" << endl;
+    Print(otherSmallArray, m / 2);
+    QuickSort(otherSmallArray, 0, m - 1);
+    cout << "Sorted array:" << endl;
+    Print(otherSmallArray, m / 2);
 
-     cout << endl;
+    cout << endl;
 
-     // Binary search
+    // Binary search
 
-     cout << "Binary search with recursion:" << endl;
-     cout << "0 is " << BSearchReq(0, smallArray, 0, m - 1) << endl << endl;;
+    cout << "//////////Binary search//////////" << endl << endl;
+    cout << "Binary search with recursion:" << endl;
+    cout << "0 is " << BSearchReq(0, smallArray, 0, m - 1) << endl << endl;;
 
-     cout << "Binary search:" << endl;
-     cout << "10 is " << BSearch(10, smallArray, 0, m - 1) << endl;
+    cout << "Binary search:" << endl;
+    cout << "10 is " << BSearch(10, smallArray, 0, m - 1) << endl;
 
-     cout << endl << endl;
+    cout << endl;
 
-     // benchmarking
+    // Benchmarking
+    cout << "//////////Benchmarking//////////" << endl << endl;
     long k = 10000;
     int* testArray = new int[k];
 
     srand(time(nullptr));
-
+    
     for (int i = 0; i < k; i++)
-        testArray[i] = rand() % 100001 - 100000;
+        testArray[i] = rand() % 100001 - 50000;
 
+   // замеряем начало работы и конец работы функции Search
     chrono::steady_clock::time_point start = chrono::steady_clock::now();
     Search(testArray, k, testArray[648]);
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
-    cout << "Search: " <<chrono::duration_cast<chrono::nanoseconds>(end - start).count() << endl;
+    // выводим время работы как разность начала и конца
+    cout << "Search time: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " ns" << endl;
 
-    QuickSort(testArray, 0, k);
+    // сортируем массив
+    QuickSort(testArray, 0, k-1);
 
+    // замеряем начало работы и конец работы функции BSearch
     start = chrono::steady_clock::now();
     BSearch(testArray[648], testArray, 0, k);
     end = chrono::steady_clock::now();
 
-    cout << "Bsearch: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    // выводим время работы как разность начала и конца
+    cout << "Bsearch time: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " ns" << endl;
 }
